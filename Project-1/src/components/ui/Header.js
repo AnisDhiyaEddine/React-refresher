be96777f4996b11df5aff89245ec6d620b37aece
ANISDHIYAEDDINE BOUDIAF <dhiaeboudiaf@gmail.com>
2021-02-26 10:22:29 +0100

    project setup and refactor

---
+++ Project-1/src/components/ui/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll(props) {
+	const { children } = props;
+
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0,
+	});
+}
+
+ElevationScroll.propTypes = {
+	children: PropTypes.element.isRequired,
+	window: PropTypes.func,
+};
+
+export default function Headers(props) {
+	return (
+		<ElevationScroll>
+			<AppBar>
+				<Toolbar>Hello khaoula</Toolbar>
+			</AppBar>
+		</ElevationScroll>
+	);
+}
